575063f913d5393760601c8b1684bf8b701149a2
gabe <gabrielliza012@gmail.com>
2024-12-02 21:56:25 -0700

    Major font changes, added pgex

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ let app = express();
 let path = require("path");
 const port = process.env.PORT || 5000;
 
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+        port: process.env.RDS_PORT,
+        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
+    }
+});
+
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
