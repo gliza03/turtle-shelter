b5cfc3bc074da79491c4995d6605a76d2ffe5678
gabe <gabrielliza012@gmail.com>
2024-12-02 21:09:49 -0700

    Finished layout for landing page (index)

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 
 app.get("/", (req, res) => res.render("index"));
+app.get("/login", (req, res) => res.render("login"));
 
 app.listen(port, () => console.log("Express App has started and server is listening!"));
