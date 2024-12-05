6b3799ad5b55c5e19c2c62038bb9327cc1a5c5b8
gabe <gabrielliza012@gmail.com>
2024-12-04 22:54:07 -0700

    HTTPS fix

--- .ebextensions/.platform/hooks/postdeploy/00_get_certificate.sh
+++ .ebextensions/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d theturtleproject.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email gabbolml@icloud.com
+sudo certbot -n -d theturtleproject.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email gabbolm@icloud.com
