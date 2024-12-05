84f01c1663e4bec7e0542612570fd6c47c89635e
gabe <gabrielliza012@gmail.com>
2024-12-05 00:06:35 -0700

    restarting https

--- .ebextensions/.platform/hooks/postdeploy/00_get_certificate.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d http://theturtleproject.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email gabbolm@icloud.com
-sudo certbot --nginx --redirect
